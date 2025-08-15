857eedbb5651d2fd97f078a87f9299433ecf872d
CoderSandeep0609 <sk0609mahi@gmail.com>
2025-08-15 23:45:06 +0530

    Use SQLite for Render deployment

--- Online_quiz/settings.py
+++ Online_quiz/settings.py
@@ -82,11 +82,13 @@ WSGI_APPLICATION = 'Online_quiz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",  # fallback for local dev
+        conn_max_age=600,  # persistent connections
+    )
 }
 
 
